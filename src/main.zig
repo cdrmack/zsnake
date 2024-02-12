16d02212febbf6985849316f2b4fd103b373510a
cdrmack <git.pynvt@passmail.net>
2024-02-12 17:48:08 +0100

    render black screen

--- src/main.zig
+++ src/main.zig
@@ -1,24 +1,22 @@
 const std = @import("std");
+const ray = @cImport({
+    @cInclude("raylib.h");
+});
 
-pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+const screen_width = 800;
+const screen_height = 600;
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+pub fn main() void {
+    ray.InitWindow(screen_width, screen_height, "zsnake");
+    defer ray.CloseWindow();
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+    ray.SetTargetFPS(60);
 
-    try bw.flush(); // don't forget to flush!
-}
+    while (!ray.WindowShouldClose()) {
+        // render
+        ray.BeginDrawing();
+        defer ray.EndDrawing();
 
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+        ray.ClearBackground(ray.BLACK);
+    }
 }
