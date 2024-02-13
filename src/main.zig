f4714c8e4d22d83f2ff2adfbfe53818713a1b595
cdrmack <git.pynvt@passmail.net>
2024-02-13 17:10:21 +0100

    render first rectangle (snake head)

--- src/main.zig
+++ src/main.zig
@@ -3,8 +3,21 @@ const ray = @cImport({
     @cInclude("raylib.h");
 });
 
-const screen_width = 800;
-const screen_height = 600;
+const screen_width = 400;
+const screen_height = 400;
+
+const snake_cell_size = 10;
+
+const Snake = struct { rectangle: ray.Rectangle, color: ray.Color };
+
+fn makeRectangle(x: f32, y: f32, width: f32, height: f32) ray.Rectangle {
+    return ray.Rectangle{
+        .x = x,
+        .y = y,
+        .width = width,
+        .height = height,
+    };
+}
 
 pub fn main() void {
     ray.InitWindow(screen_width, screen_height, "zsnake");
@@ -12,11 +25,18 @@ pub fn main() void {
 
     ray.SetTargetFPS(60);
 
+    const snake = Snake{
+        .rectangle = makeRectangle(50, 50, snake_cell_size, snake_cell_size),
+        .color = ray.GREEN,
+    };
+
     while (!ray.WindowShouldClose()) {
         // render
         ray.BeginDrawing();
         defer ray.EndDrawing();
 
         ray.ClearBackground(ray.BLACK);
+
+        ray.DrawRectangleRec(snake.rectangle, snake.color);
     }
 }
