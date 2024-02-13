448e20fe13a0081739bf28b93bcdb5f2d24baba6
cdrmack <git.pynvt@passmail.net>
2024-02-13 17:21:01 +0100

    update snake position based on the current direction

--- src/main.zig
+++ src/main.zig
@@ -8,7 +8,9 @@ const screen_height = 400;
 
 const snake_cell_size = 10;
 
-const Snake = struct { rectangle: ray.Rectangle, color: ray.Color };
+const Direction = enum { up, down, left, right };
+
+const Snake = struct { rectangle: ray.Rectangle, direction: Direction, color: ray.Color };
 
 fn makeRectangle(x: f32, y: f32, width: f32, height: f32) ray.Rectangle {
     return ray.Rectangle{
@@ -19,18 +21,39 @@ fn makeRectangle(x: f32, y: f32, width: f32, height: f32) ray.Rectangle {
     };
 }
 
+fn snakeTick(snake: *Snake) void {
+    switch (snake.direction) {
+        Direction.up => {
+            snake.rectangle.y -= snake_cell_size;
+        },
+        Direction.down => {
+            snake.rectangle.y += snake_cell_size;
+        },
+        Direction.left => {
+            snake.rectangle.x -= snake_cell_size;
+        },
+        Direction.right => {
+            snake.rectangle.x += snake_cell_size;
+        },
+    }
+}
+
 pub fn main() void {
     ray.InitWindow(screen_width, screen_height, "zsnake");
     defer ray.CloseWindow();
 
     ray.SetTargetFPS(60);
 
-    const snake = Snake{
+    var snake = Snake{
         .rectangle = makeRectangle(50, 50, snake_cell_size, snake_cell_size),
+        .direction = Direction.down,
         .color = ray.GREEN,
     };
 
     while (!ray.WindowShouldClose()) {
+        // tick
+        snakeTick(&snake);
+
         // render
         ray.BeginDrawing();
         defer ray.EndDrawing();
